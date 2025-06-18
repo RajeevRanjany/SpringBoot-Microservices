0885259cdb1bfb69f6faa33b1a0823e333857819
Rajeev-Ranjanc <itrajeevranjan@gmail.com>
2025-06-18 15:58:40 +0530

    cart realted job done

---
+++ ecom-microservices/order/src/main/java/com/ecommerce/order/repository/CartItemRepository.java
@@ -0,0 +1,23 @@
+package com.ecommerce.order.repository;
+
+import com.app.ecom.model.CartItem;
+import com.app.ecom.model.Product;
+import com.app.ecom.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    CartItem findByUserAndProduct(User user, Product product);
+
+    void deleteByUserAndProduct(User user, Product product);
+
+    CartItem findCartItemById(Long id);
+
+    List<CartItem> findByUser(User user);
+
+    void deleteByUser(User user);
+
+}
---
+++ ecom-microservices/product/src/main/java/com/ecommerce/product/controllers/ProductController.java
@@ -0,0 +1,46 @@
+package com.ecommerce.product.controllers;
+
+import com.app.ecom.dto.ProductRequest;
+import com.app.ecom.dto.ProductResponse;
+import com.app.ecom.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    private ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest ProductRequest) {
+        return new ResponseEntity<ProductResponse>(productService.createProduct(ProductRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    private ResponseEntity<List<ProductResponse>> getProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @PutMapping("{id}")
+    private ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
+        return productService.updateProduct(id, productRequest).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        boolean deleted = productService.deleteProduct(id);
+        return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/search")
+    private ResponseEntity<List<ProductResponse>> searchProducts(@RequestParam String keyword) {
+        return ResponseEntity.of(Optional.ofNullable(productService.searchProducts(keyword)));
+    }
+
+}
