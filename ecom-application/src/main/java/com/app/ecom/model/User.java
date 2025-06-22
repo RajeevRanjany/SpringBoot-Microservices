b67b0ad43d1d8c26fb270a802632dba204954630
Rajeev-Ranjanc <itrajeevranjan@gmail.com>
2025-06-22 20:39:35 +0530

    the refreshbus added

--- ecom-application/src/main/java/com/app/ecom/model/User.java
+++ ecom-application/src/main/java/com/app/ecom/model/User.java
@@ -1,12 +1,13 @@
-package com.app.ecom;
+package com.app.ecom.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -16,10 +17,20 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id = 1L;
+    private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
     private UserRole role = UserRole.CUSTOMER;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id",
+            referencedColumnName = "id")
+    private Address address;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
